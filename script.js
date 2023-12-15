0175c14ad4157c60d3265438344bf8a1fa6b5f87
Wagner Oliveira <74685635+WagProjects@users.noreply.github.com>
2023-12-15 16:53:27 -0300

    focus fixed

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 3
+    tempoDecorridoEmSegundos = 60
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
